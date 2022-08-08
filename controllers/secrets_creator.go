cc5a0e93ef581859628d11ae24f8a93b00852f95
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2022-08-08 08:24:16 +0300

    #108 - refactoring secrets_creator controller

--- controllers/secrets_creator.go
+++ controllers/secrets_creator.go
@@ -15,40 +15,49 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func (c *controller) checkDstNS(dstNS string) (string, error) {
-	if _, err := c.ClientSet.CoreV1().Namespaces().Get(context.TODO(), dstNS, metaV1.GetOptions{}); errors.IsNotFound(err) {
-		klog.Infof("Namespace: %s is not created", dstNS)
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		return "", fmt.Errorf("Error getting Namespace %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		return "", err
+func (c *controller) iteratorDstNS(dstNamespaces []string, handlerSecret func(ns string) error) error {
+	for _, ns := range dstNamespaces {
+		if _, err := c.ClientSet.CoreV1().Namespaces().Get(context.TODO(), ns, metaV1.GetOptions{}); errors.IsNotFound(err) {
+			klog.Infof("Namespace: %s is not created", ns)
+			continue
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			return fmt.Errorf("Error getting Namespace %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			return err
+		}
+
+		if err := handlerSecret(ns); err != nil {
+			return err
+		}
 	}
 
-	return dstNS, nil
+	return nil
 }
 
 func (c *controller) deleteDstSecrets(key string) error {
 	for _, secret := range c.SecretList.Secrets {
 		if secret.SrcNamespace+"/"+secret.Name == key {
 			klog.Infof("Src secret %s does not exist anymore\n", key)
-			for _, ns := range secret.DstNamespaces {
-				dstNamespace, err := c.checkDstNS(ns)
-				if err != nil {
-					return err
-				}
+			if err := c.iteratorDstNS(
+				secret.DstNamespaces,
+				func(ns string) error {
+					dstSecret, err := c.ClientSet.CoreV1().Secrets(ns).Get(context.TODO(), secret.Name, metaV1.GetOptions{})
+					if errors.IsNotFound(err) {
+						return nil
+					} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+						return fmt.Errorf("Error getting Secret %v\n", statusError.ErrStatus.Message)
+					} else if err != nil {
+						return err
+					}
 
-				dstSecret, err := c.ClientSet.CoreV1().Secrets(dstNamespace).Get(context.TODO(), secret.Name, metaV1.GetOptions{})
-				if errors.IsNotFound(err) {
-					continue
-				} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-					return fmt.Errorf("Error getting Secret %v\n", statusError.ErrStatus.Message)
-				} else if err != nil {
-					return err
-				}
+					if err := c.ClientSet.CoreV1().Secrets(ns).Delete(context.TODO(), dstSecret.GetName(), metaV1.DeleteOptions{}); err != nil {
+						return err
+					}
 
-				if err := c.ClientSet.CoreV1().Secrets(ns).Delete(context.TODO(), dstSecret.GetName(), metaV1.DeleteOptions{}); err != nil {
-					return err
-				}
+					return nil
+				},
+			); err != nil {
+				return err
 			}
 		}
 	}
@@ -67,44 +76,52 @@ func (c *controller) createUpdateDstSecrets(obj interface{}) error {
 	for _, secret := range c.SecretList.Secrets {
 		if secret.Name == srcSecret.GetName() && secret.SrcNamespace == srcSecret.GetNamespace() {
 			klog.Infof("Sync/Add for secret %s, namespace: %s\n", srcSecret.GetName(), srcSecret.GetNamespace())
-			for _, ns := range secret.DstNamespaces {
-				dstNamespace, err := c.checkDstNS(ns)
-				if err != nil {
-					return err
-				}
+			if err := c.iteratorDstNS(
+				secret.DstNamespaces,
+				func(ns string) error {
+					newSecret := srcSecret.DeepCopy()
+					newSecret.SetNamespace(ns)
+					newSecret.ObjectMeta.OwnerReferences = []metaV1.OwnerReference{}
+					newSecret.Labels = map[string]string{}
 
-				newSecret := srcSecret.DeepCopy()
-				newSecret.SetNamespace(dstNamespace)
-				newSecret.ObjectMeta.OwnerReferences = []metaV1.OwnerReference{}
-				newSecret.Labels = map[string]string{}
+					newSecret.Annotations = make(map[string]string)
+					newSecret.Annotations[types.SyncAtAnnotation] = time.Now().Format(time.RFC3339)
+					newSecret.Annotations[types.SyncFromVersionAnnotation] = srcSecret.ResourceVersion
+					newSecret.Annotations[types.SyncKeysAnnotation] = strings.Join(keys, ",")
 
-				newSecret.Annotations[types.SyncAtAnnotation] = time.Now().Format(time.RFC3339)
-				newSecret.Annotations[types.SyncFromVersionAnnotation] = srcSecret.ResourceVersion
-				newSecret.Annotations[types.SyncKeysAnnotation] = strings.Join(keys, ",")
+					dstSecret, err := c.ClientSet.CoreV1().Secrets(ns).Get(context.TODO(),
+						newSecret.GetName(), metaV1.GetOptions{})
+					if errors.IsNotFound(err) {
+						newSecret.ObjectMeta.ResourceVersion = ""
+						if _, err := c.ClientSet.CoreV1().Secrets(ns).Create(context.TODO(),
+							newSecret, metaV1.CreateOptions{}); err != nil {
+							return err
+						}
 
-				dstSecret, err := c.ClientSet.CoreV1().Secrets(dstNamespace).Get(context.TODO(), newSecret.GetName(), metaV1.GetOptions{})
-				if errors.IsNotFound(err) {
-					newSecret.ObjectMeta.ResourceVersion = ""
-					if _, err := c.ClientSet.CoreV1().Secrets(dstNamespace).Create(context.TODO(), newSecret, metaV1.CreateOptions{}); err != nil {
+						klog.Infof("Created dst secret %s in dst namespace: %s",
+							newSecret.GetName(), newSecret.GetNamespace())
+					} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+						return fmt.Errorf("Error getting Secrets %v\n", statusError.ErrStatus.Message)
+					} else if err != nil {
 						return err
+					} else {
+						if !reflect.DeepEqual(dstSecret.Data, newSecret.Data) {
+							dstSecret.Data = newSecret.Data
+							_, err := c.ClientSet.CoreV1().Secrets(ns).Update(context.TODO(),
+								dstSecret, metaV1.UpdateOptions{})
+							if err != nil {
+								return err
+							}
+
+							klog.Infof("Update data dst secret %s in dst namespace: %s",
+								newSecret.GetName(), newSecret.GetNamespace())
+						}
 					}
 
-					klog.Infof("Created dst secret %s in dst namespace: %s", newSecret.GetName(), newSecret.GetNamespace())
-				} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-					return fmt.Errorf("Error getting Secrets %v\n", statusError.ErrStatus.Message)
-				} else if err != nil {
-					return err
-				}
-
-				if !reflect.DeepEqual(dstSecret.Data, newSecret.Data) {
-					dstSecret.Data = newSecret.Data
-					_, err := c.ClientSet.CoreV1().Secrets(ns).Update(context.TODO(), dstSecret, metaV1.UpdateOptions{})
-					if err != nil {
-						return err
-					}
-
-					klog.Infof("Update data dst secret %s in dst namespace: %s", newSecret.GetName(), newSecret.GetNamespace())
-				}
+					return nil
+				},
+			); err != nil {
+				return err
 			}
 		}
 	}
